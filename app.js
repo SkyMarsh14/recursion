5c84b1729d65488233f46e7646ac52fca34edae4
Sky Randy <soras1758@gmail.com>
2024-09-22 09:39:38 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+function fibs(n){
+    if(n<=2) return n;
+    let a =0;
+    let b=1;
+    let c;
+    let arr=[a,b];
+    for(let i=2;i<n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+        arr.push(b);
+    }
+    return arr;
+}
+
+function fibsRec(n, sequence=[0,1]){
+    console.log("This was printed recursively")
+    if(n<=2) return sequence;
+    else if (n===1) return [0];
+
+    sequence.push(sequence[sequence.length-1]+sequence[sequence.length-2]);
+    return fibsRec(n-1,sequence);
+}
+
+console.log(fibsRec(8));
---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+function mergeSort(arr){
+
+    if(arr.length<=1) return arr;
+
+    let [left,right]=split(arr);
+
+    left = mergeSort(left);
+    right=mergeSort(right);
+
+    return merge(left,right);
+}
+
+function split(array){
+    let half=Math.floor(array.length/2);
+
+    return [array.slice(0,half),array.slice(half)];
+}
+
+function merge(left,right,result=[]){
+
+    if(left.length===0) return result.concat(right);
+    if(right.length===0) return result.concat(left);
+
+    if(left[0]<right[0]){
+        result.push(left.shift());
+    }else{
+        result.push(right.shift());
+    }
+    return merge(left,right,result);
+}
+
+let arr=[0,3,5,10,1];
+console.log(mergeSort(arr))
